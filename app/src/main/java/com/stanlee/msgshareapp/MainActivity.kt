68471f4e4ab7980b70f85ce4f000b55141563e50
Stanley Umezuruike <manstylo@gmail.com>
2020-02-21 18:20:17 +0100

    ShowToast Button

---
+++ app/src/main/java/com/stanlee/msgshareapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.stanlee.msgshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "Button was clicked!")
+            Toast.makeText(this, "BUTTON WAS CLICKED!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
+
+
+
