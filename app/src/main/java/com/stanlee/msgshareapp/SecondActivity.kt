cc35da7de34f4c2b1d1373876ad9f1b10d3cf884
Stanley Umezuruike <manstylo@gmail.com>
2020-02-22 13:18:31 +0100

    Implicit Intent

--- app/src/main/java/com/stanlee/msgshareapp/SecondActivity.kt
+++ app/src/main/java/com/stanlee/msgshareapp/SecondActivity.kt
@@ -1,7 +1,9 @@
 package com.stanlee.msgshareapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.ShareActionProvider
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -14,5 +16,15 @@ class SecondActivity : AppCompatActivity() {
         val msg = bundle!!.getString("key")
 
         txtUserMsg.text = msg
+
+
+        btnShare.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, msg)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Share to: "))
+        }
     }
 }
